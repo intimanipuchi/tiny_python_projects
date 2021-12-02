af9144f2c2393330ddf5c11f9cc05c8a23867906
intimanipuchi <intimanipuchi@gmail.com>
2021-12-01 23:53:11 -0600

    Finish Chapter 1, hello.py complete

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,1 +1,30 @@
-print("Hello, World!")
+#!/usr/bin/env python3
+"""
+Author: Roman Koziy following the Tiny Python Projects book by Ken
+Youens-Clark <kyclark@gmail.com
+Purpose: Say hello
+"""
+
+import argparse
+
+
+def get_args():
+    """Get command line arguments."""
+
+    parser = argparse.ArgumentParser(description="Say hello")
+    parser.add_argument("-n",
+                        "--name",
+                        metavar="name",
+                        default="World",
+                        help="Name to greet")
+    return parser.parse_args()
+
+
+def main():
+    """Main function to execute."""
+    args = get_args()
+    print(f"Hello, {args.name}!")
+
+
+if __name__ == "__main__":
+    main()
