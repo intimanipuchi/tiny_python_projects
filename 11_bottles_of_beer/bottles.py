30cb3690b0c7bfddd4ebd4c1db3ce966ce8a04af
Intimanipuchi <intimanipuchi@gmail.com>
2022-02-14 16:11:02 -0600

    Create verse function and unit test

--- 11_bottles_of_beer/bottles.py
+++ 11_bottles_of_beer/bottles.py
@@ -36,6 +36,32 @@ def main():
     args = get_args()
     print(args.num)
 
+def verse(bottle):
+    """Sing a verse"""
+
+    return '\n'.join([
+       '1 bottle of beer on the wall,', '1 bottle of beer,',
+       'Take one down, pass it around,',
+       'No more bottles of beer on the wall!'
+    ])
+
+def test_verse():
+    """Test verse"""
+
+    last_verse = verse(1)
+    assert last_verse == '\n'.join([
+       '1 bottle of beer on the wall,', '1 bottle of beer,',
+       'Take one down, pass it around,',
+       'No more bottles of beer on the wall!'
+    ])
+
+    two_bottles = verse(2)
+    assert two_bottles == '\n'.join([
+        '2 bottles of beer on the wall,', '2 bottles of beer,',
+        'Take one down, pass it around,',
+        '1 bottle of beer on the wall!'
+    ])
+
 # --------------------------------------------------
 if __name__ == '__main__':
     main()
